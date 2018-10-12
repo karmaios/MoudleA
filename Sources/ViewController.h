4c0115f71d0893ec386a2b82bbf90584d7bd52df
Haiwen <haiwen@vip.com>
2018-10-12 17:50:34 +0800

    新增Sources

---
+++ Sources/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoudleA
+//
+//  Created by 刘海文 on 2018/10/12.
+//  Copyright © 2018 Netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sources/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MoudleA
+//
+//  Created by 刘海文 on 2018/10/12.
+//  Copyright © 2018 Netease. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
